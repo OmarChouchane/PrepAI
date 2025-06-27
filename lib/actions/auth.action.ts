0fc33de2b0fa192247f4c77283a1ae1655571399
OmarChouchane <omar.ch52831@gmail.com>
2025-06-28 00:22:22 +0100

    auth and storage

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -91,3 +91,38 @@ export async function setSessionCookie(idToken: string) {
         path: '/',
     });
 }
+
+export async function getCurrentUser(): Promise<User | null> {
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get('session')?.value;
+
+    if (!sessionCookie) {
+        return null;
+    }
+
+    try {
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+        const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+
+        if (!userRecord.exists) {
+            return null;
+        }
+
+        return {
+            ...userRecord.data(),
+            id: decodedClaims.uid,
+        }as User;
+
+        
+    } catch (error) {
+        console.error('Error getting current user', error);
+        return null;
+    }
+}
+
+export async function isAuthenticated(): Promise<boolean> {
+
+    const user = await getCurrentUser();
+    return !!user;
+}   
