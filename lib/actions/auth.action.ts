4f5e278c61081e1fdd84007e43f026b15d2e54c7
OmarChouchane <omar.ch52831@gmail.com>
2025-06-27 23:21:11 +0100

    sign in action

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,9 @@
 'use server'
 
-import { db } from '@/firebase/admin';
+import { db, auth } from '@/firebase/admin';
+import { cookies } from 'next/headers';
+
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000; // 1 week in milliseconds
 
 export async function SignUp(params: SignUpParams){
     const { uid, name, email} = params;
@@ -35,4 +38,46 @@ export async function SignUp(params: SignUpParams){
             message: 'An error occurred while creating the user. Please try again later.'
         }
     }
+}
+
+export async function SignIn(params: SignInParams) {
+    const { email, idToken } = params;
+
+    try {
+
+        const userRecord = await auth.getUserByEmail(email);
+
+        if (!userRecord) {
+            return {
+                success: false,
+                message: 'User not found. Please sign up first.'
+            };
+        }
+
+        await setSessionCookie(idToken);
+        
+    } catch (error) {
+        console.error('Error signing in', error);
+        return {
+            success: false,
+            message: 'An error occurred while signing in. Please try again later.'
+        }
+    }
+}
+
+export async function setSessionCookie(idToken: string) {
+    const cookieStore = await cookies();
+    const sessionCookies = await auth.createSessionCookie(idToken, {
+      expiresIn: ONE_WEEK
+    });
+
+    cookieStore.set({
+        name: 'session',
+        value: sessionCookies,
+        httpOnly: true,
+        maxAge: ONE_WEEK,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+    });
 }
