7ce87c1e4c2769770a43026de32280197354e66e
OmarChouchane <omar.ch52831@gmail.com>
2025-07-04 03:32:23 +0100

    feedback page init

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -1,6 +1,19 @@
+import { RouteParams } from '@/types'
 import React from 'react'
+import { GetFeedbackByInterviewId, getInterviewById } from '@/lib/actions/general.action'
+import { redirect } from 'next/navigation';
+import { getCurrentUser } from '@/lib/actions/auth.action';
 
-const page = () => {
+const page = async ({ params }: RouteParams) => {
+
+  const { id } = await params;
+  const user = await getCurrentUser();
+  const interview = await getInterviewById(id);
+
+  if(!interview || !user?.id) redirect('/');
+
+  const feedback = await GetFeedbackByInterviewId({ interviewId: id, userId: user.id });
+
   return (
     <div>
       
